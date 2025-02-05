2a1476c0dc2ae26a5609d108a6cd656a147cddc5
mohsen <71mohsenpsh@gmail.com>
2025-02-05 12:16:34 +0330

    Add async repository interfaces for Product management
    Introduced two new interfaces in the `ProductManagement.Application.Contracts.Persistence` namespace.
    
    - Added `IAsyncRepository<T>` for generic async CRUD operations.
    - Added `IProductsRepository` extending `IAsyncRepository<Product>` with an additional method to get orders by user name.

---
+++ Services/ProductManagement/ProductManagement.Application/Contracts/Persistence/IProductsRepository.cs
@@ -0,0 +1,14 @@
+﻿using ProductManagement.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductManagement.Application.Contracts.Persistence
+{
+    public interface IProductsRepository: IAsyncRepository<Product>
+    {
+        Task<IEnumerable<Product>> GetOrdersByUserName(string userName);
+    }
+}
