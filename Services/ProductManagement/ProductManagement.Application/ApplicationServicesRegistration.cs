2b3c2f954a6ddd8759a007b06aeed8a0638954cf
mohsen <71mohsenpsh@gmail.com>
2025-02-05 15:02:05 +0330

    Add DI configuration for AutoMapper and MediatR
    Introduce ConfigureApplicationServices method in ApplicationServicesRegistration.cs to set up AutoMapper and MediatR. Update Program.cs to use this method for configuring application services.

---
+++ Services/ProductManagement/ProductManagement.Application/ApplicationServicesRegistration.cs
@@ -0,0 +1,16 @@
+﻿
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace ProductManagement.Application
+{
+    public static class ApplicationServicesRegistration
+    {
+        public static void ConfigureApplicationServices(this IServiceCollection services)
+        {
+            // services.AddAutoMapper(typeof(MappingProfile));
+            services.AddAutoMapper(Assembly.GetExecutingAssembly());
+            services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly()));
+        }
+    }
+}
