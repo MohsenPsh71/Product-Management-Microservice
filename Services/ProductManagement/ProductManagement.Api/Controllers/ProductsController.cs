753e7df64a84438b34a6b29061abe82f3407b79e
mohsen <71mohsenpsh@gmail.com>
2025-02-05 17:21:44 +0330

    Add ProductsController and update project dependencies
    Added ProductsController.cs to handle product operations using MediatR.
    Endpoints include GET, POST, PUT, and DELETE for products.
    Updated ProductManagement.Api.csproj to include Microsoft.VisualStudio.Web.CodeGeneration.Design package and removed redundant folder item group.

---
+++ Services/ProductManagement/ProductManagement.Api/Controllers/ProductsController.cs
@@ -0,0 +1,54 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ProductManagement.Application.Features.Products.Requests.Commands;
+using ProductManagement.Application.Features.Products.Requests.Queries;
+using ProductManagement.Domain.Entities;
+
+namespace ProductManagement.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+
+        private readonly IMediator _mediator;
+
+        public ProductsController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+      
+        [HttpGet]
+        public async Task<IActionResult> GetProducts()
+        {
+            var products = await _mediator.Send(new GetAllProductsRequest());
+            return Ok(products);
+        }
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetProduct(Guid id)
+        {
+            var product = await _mediator.Send(new GetProductByIdRequest { Id = id });
+            return Ok(product);
+        }
+        [HttpPost]
+        public async Task<IActionResult> CreateProduct([FromBody] CreateProductsCommand command)
+        {
+            var response = await _mediator.Send(command);
+            return Ok(response);
+        }
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateProduct(Guid id, [FromBody] UpdateProductsCommand command)
+        {
+            var updatedProduct = await _mediator.Send(command);
+            return NoContent();
+        }
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteProduct(Guid id)
+        {
+            await _mediator.Send(new DeleteProductsCommand { Id = id });
+            return NoContent();
+        }
+    }
+}
