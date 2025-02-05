44c9d6c44187d0d8b9ce180cec3eb86e4534a262
mohsen <71mohsenpsh@gmail.com>
2025-02-05 17:22:40 +0330

    Add Id property to CreateProductsCommand class
    The CreateProductsCommand class in the ProductManagement.Application.Features.Products.Requests.Commands namespace now includes a new property named Id of type Guid. This property is initialized with a new Guid value to uniquely identify each product creation command.

--- Services/ProductManagement/ProductManagement.Application/Features/Products/Requests/Commands/CreateProductsCommand.cs
+++ Services/ProductManagement/ProductManagement.Application/Features/Products/Requests/Commands/CreateProductsCommand.cs
@@ -4,6 +4,7 @@ namespace ProductManagement.Application.Features.Products.Requests.Commands
 {
     public class CreateProductsCommand: IRequest<Guid>
     {
+        public Guid Id { get; set; } = new Guid();
         public string Name { get; set; }
         public decimal Price { get; set; }
         public int StockQuantity { get; set; }
