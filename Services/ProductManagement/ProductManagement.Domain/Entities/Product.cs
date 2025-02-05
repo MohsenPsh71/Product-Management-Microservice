2d1401f14f0ebaed94c8f639f8dc956f65538776
mohsen <71mohsenpsh@gmail.com>
2025-02-05 11:50:31 +0330

    Add Product class with properties and primary key attribute
    Introduced a new Product class in the ProductManagement.Domain.Entities namespace. This class includes properties for Id, Name, Price, and StockQuantity. The Id property is decorated with the [Key] attribute to designate it as the primary key. Imported System.ComponentModel.DataAnnotations to support data annotations.

---
+++ Services/ProductManagement/ProductManagement.Domain/Entities/Product.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ProductManagement.Domain.Entities
+{
+    public class Product
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public int StockQuantity { get; set; }
+
+    }
+}
