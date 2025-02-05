0d478c6c2877e7ed43c7f15eb77bc5ee5050c475
mohsen <71mohsenpsh@gmail.com>
2025-02-05 17:23:30 +0330

    Add new AutoMapper mappings for CreateProductsCommand
    Updated MappingProfile.cs to include mappings for CreateProductsCommand.
    Added necessary using directives for Product and CreateProductsCommand.
    Existing mappings for UpdateProductsCommand remain unchanged.

--- Services/ProductManagement/ProductManagement.Application/Profiles/MappingProfile.cs
+++ Services/ProductManagement/ProductManagement.Application/Profiles/MappingProfile.cs
@@ -1,13 +1,15 @@
 ﻿
 using AutoMapper;
+using ProductManagement.Application.Features.Products.Requests.Commands;
+using ProductManagement.Domain.Entities;
 
 namespace ProductManagement.Application.Profiles
 {
     internal class MappingProfile : Profile
     {
         public MappingProfile() {
-
-
+            CreateMap<Product, UpdateProductsCommand>().ReverseMap();
+            CreateMap<Product, CreateProductsCommand>().ReverseMap();
         }
     }
 }
